2cd8595b1ead1ff93f31435b9de22543f44b6ec3
Zack Craig <nagada10@aol.com>
2015-11-04 14:18:50 -0500

    to do

--- js/app.js
+++ js/app.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
 	})
 });
 
-//	$("#todo").click(function() {
-//		$(".list").append("1");
-//	})
-//});
+//jQuery:
+// make input box blank after clicking add-button
+// new item needs to go to new line
+// strikethrough after clicking on item
+// clear whole list when done??
+
+//CSS:
+//align items on lines
